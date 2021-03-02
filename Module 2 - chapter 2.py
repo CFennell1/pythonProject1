590dc6156aef1d3827df51d59f30f49231912de1
CFennell1 <colin.fennell@zurich.com>
2021-03-02 20:21:56 +0000

    1st commit

--- Module 2 - chapter 2.py
+++ Module 2 - chapter 2.py
@@ -205,3 +205,60 @@ print(cars.iloc[:,[2]])
 print(cars)
 # Print out cars_per_cap and drives_right as DataFrame
 print(cars.iloc[:,[0,2]])
+
+##############################################
+# Import cars data
+import pandas as pd
+cars = pd.read_csv('cars.csv', index_col = 0)
+
+# Extract drives_right column as Series: dr
+dr=cars["drives_right"]
+
+
+# Use dr to subset cars: sel
+sel=cars[dr]
+
+# Print sel
+print(sel)
+
+############################################
+# Import cars data
+import pandas as pd
+cars = pd.read_csv('cars.csv', index_col = 0)
+
+# Convert code to a one-liner
+
+sel=cars[cars['drives_right']]
+
+# Print sel
+print(sel)
+##############################################
+# Import cars data
+import pandas as pd
+cars = pd.read_csv('cars.csv', index_col = 0)
+
+# Create car_maniac: observations that have a cars_per_cap over 500
+cpc=cars['cars_per_cap']
+
+many_cars=cpc>500
+
+car_maniac = cars[many_cars]
+
+# Print car_maniac
+print(car_maniac)
+###########################################
+# Import cars data
+import pandas as pd
+cars = pd.read_csv('cars.csv', index_col = 0)
+
+# Import numpy, you'll need this
+import numpy as np
+
+# Create medium: observations with cars_per_cap between 100 and 500
+cpc=cars['cars_per_cap']
+between=np.logical_and(cpc>100,cpc<500)
+medium = cars[between]
+
+
+# Print medium
+print(medium)
