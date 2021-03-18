b038f877f567f58b584ab3c0376dd9f499bfe515
COLIN.FENNELL1 <colin.fennell@zurich.com>
2021-03-18 20:05:24 +0000

    another commit

--- Module 5 - chapter 2.py
+++ Module 5 - chapter 2.py
@@ -32,3 +32,65 @@ xls = pd.ExcelFile(file)
 
 # Print sheet names
 print(xls.sheet_names)
+
+###############################
+# Load a sheet into a DataFrame by name: df1
+df1 = xls.parse('2004')
+
+# Print the head of the DataFrame df1
+print(df1.head())
+
+# Load a sheet into a DataFrame by index: df2
+df2 = xls.parse(0)
+
+# Print the head of the DataFrame df2
+print(df2.head())
+
+#################################
+# Parse the first sheet and rename the columns: df1
+df1 = xls.parse(0, skiprows=1, names=["Country", "AAM due to War (2002)"])
+
+# Print the head of the DataFrame df1
+print(df1.head())
+
+# Parse the first column of the second sheet and rename the column: df2
+df2 = xls.parse(1, usecols=[0], skiprows=1, names=["Country"])
+
+# Print the head of the DataFrame df2
+print(df2.head())
+
+####################################
+# Import sas7bdat package
+from sas7bdat import SAS7BDAT
+
+# Save file to a DataFrame: df_sas
+with SAS7BDAT('sales.sas7bdat') as file:
+    df_sas = file.to_data_frame()
+
+# Print head of DataFrame
+print(df_sas.head())
+
+# Plot histogram of DataFrame features (pandas and pyplot already imported)
+import matplotlib.pyplot as plt
+pd.DataFrame.hist(df_sas[['P']])
+plt.ylabel('count')
+plt.show()
+
+######################
+#df = pd.read_stata('disarea.dta')
+###################
+
+# Import pandas
+import pandas as pd
+
+# Load Stata file into a pandas DataFrame: df
+df=pd.read_stata('disarea.dta')
+
+# Print the head of the DataFrame df
+print(df.head())
+
+# Plot histogram of one column of the DataFrame
+pd.DataFrame.hist(df[['disa10']])
+plt.xlabel('Extent of disease')
+plt.ylabel('Number of countries')
+plt.show()
