3224c4d50fc0b4016f5f6083461e1475f0ce4d37
COLIN.FENNELL1 <colin.fennell@zurich.com>
2021-04-18 10:37:58 +0100

    another commit

--- Project.py
+++ Project.py
@@ -52,7 +52,8 @@ print(df.columns)
 # Get the total number of avocados sold of each size
 #nb_sold_by_size=avocados.groupby("size")["nb_sold"].sum()
 
-df1 = df[df["numberPickOverall"]==1]
+#df1 = df[df["numberPickOverall"]==1]
+df1 = df[df["yearDraft">1969] and df["numberPickOverall"==1]]
 
 #df1=df(numerPickOverall ==1)
 print(df1.head())
@@ -60,7 +61,31 @@ print(df1.shape)
 print(df1["namePlayer"])
 print(df1['nameTeam'])
 
-num1_by_team = df1.groupby("nameTeam")["numberPickOverall"].sum()
-print(num1_by_team)
+#df2= df1.groupby("nameTeam")["numberPickOverall"].sum()
+df2= df1.groupby("nameTeam")["numberPickOverall"].sum()
+
+#num1_by_team = num1_by_team.sort_values("numberPickOverall",ascending = False)
+print(df2)
+
+
+import matplotlib.pyplot as plt
+
+df2.plot(x="team",kind="bar", title="Number 1 picks by Team")
+plt.show()
+
+#vocados.plot(x="nb_sold", y ="avg_price", kind = "scatter", title="Number of avocados sold vs. average price")
+
+
+# Plot a bar-chart of gold medals as a function of country
+#ax.bar(num1_by_team["nameTeam"])
+
+# Set the x-axis tick labels to the country names
+#ax.set_xticklabels(medals.index, rotation = 90)
+
+# Set the y-axis label
+#ax.set_ylabel("Number of medals")
+
+#plt.show()
+
 
 #merge this to the teams
