f16b5101cf9c261baa124dae542efbe0bc1ebc2b
COLIN.FENNELL1 <colin.fennell@zurich.com>
2021-04-18 12:22:12 +0100

    another commit

--- Project.py
+++ Project.py
@@ -44,16 +44,16 @@ rs=con.execute(("Select * from Draft"))
 df = pd.DataFrame(rs.fetchall())
 df.columns=rs.keys()
 # Close connection
-con.close()
+#con.close()
 print(df.head())
 print(df.shape)
 print(df.columns)
 
-# Get the total number of avocados sold of each size
-#nb_sold_by_size=avocados.groupby("size")["nb_sold"].sum()
 
-#df1 = df[df["numberPickOverall"]==1]
-df1 = df[df["yearDraft">1969] and df["numberPickOverall"==1]]
+
+df1 = df[df["numberPickOverall"]==1]
+df1=df1[df1["yearDraft"]>1970]
+#df1 = df[df["yearDraft">=1970] and df["numberPickOverall"==1]]
 
 #df1=df(numerPickOverall ==1)
 print(df1.head())
@@ -63,29 +63,28 @@ print(df1['nameTeam'])
 
 #df2= df1.groupby("nameTeam")["numberPickOverall"].sum()
 df2= df1.groupby("nameTeam")["numberPickOverall"].sum()
+df2.columns=['Team_Name', 'Count_of_Number_1_Picks']
 
 #num1_by_team = num1_by_team.sort_values("numberPickOverall",ascending = False)
 print(df2)
+print(df2.columns)
 
 
 import matplotlib.pyplot as plt
 
-df2.plot(x="team",kind="bar", title="Number 1 picks by Team")
+df2.plot(x="Team Name", y ="Count of Number 1 Picks",kind="bar", title="Number 1 picks by Team")
 plt.show()
-
-#vocados.plot(x="nb_sold", y ="avg_price", kind = "scatter", title="Number of avocados sold vs. average price")
-
-
-# Plot a bar-chart of gold medals as a function of country
-#ax.bar(num1_by_team["nameTeam"])
-
-# Set the x-axis tick labels to the country names
-#ax.set_xticklabels(medals.index, rotation = 90)
-
-# Set the y-axis label
-#ax.set_ylabel("Number of medals")
-
-#plt.show()
 
 
 #merge this to the teams
+rs=con.execute(("Select * from Team"))
+df_team = pd.DataFrame(rs.fetchall())
+df_team.columns=rs.keys()
+# Close connection
+
+print(df_team.head())
+df_team_updated = df_team.merge(df2,left_on="full_name",right_on="nameTeam",)
+print(df_team_updated.head())
+print(df_team_updated.shape)
+
+#taxi_own_veh = taxi_owners.merge(taxi_veh, on='vid', suffixes=('_own','_veh'))
